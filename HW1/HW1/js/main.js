69a5d616e4b72922e99d77675c66d1b956b6e3d3
kellysoh <so37@students.calvin.edu>
2018-09-28 00:42:17 -0400

    Add Lab4

---
+++ HW1/HW1/js/main.js
@@ -0,0 +1,9 @@
+﻿//basic route
+var express = require('express')
+var app = express()
+//respond with "Hello"
+app.get('/', function (req, res) {
+    res.send('Hello!')
+})
+
+//res.sendStatus(404); //equivalent to res.status (404).send('Not Found')
